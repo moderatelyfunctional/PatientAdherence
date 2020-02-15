c58b640f36623efb7eb81fc9581e0953a5eb9a03
Jing Lin <moderatelyfunctionalprogrammer@gmail>
2020-02-15 09:52:23 -0800

    first frontend commit

---
+++ PatientAdherence/PatientAdherence/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.swift
+//  PatientAdherence
+//
+//  Created by Jing Lin on 2/15/20.
+//  Copyright © 2020 Jing Lin. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = UIColor.green
+    }
+
+
+}
+
