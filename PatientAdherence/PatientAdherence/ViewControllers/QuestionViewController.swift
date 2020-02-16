0e0dc132e53fec061b8f0fb0aea6bb27440cf800
Jing Lin <moderatelyfunctionalprogrammer@gmail>
2020-02-16 03:29:46 -0800

    @#)0$ speech to text

--- PatientAdherence/PatientAdherence/ViewControllers/QuestionViewController.swift
+++ PatientAdherence/PatientAdherence/ViewControllers/QuestionViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Speech
 import AVFoundation
 
 class QuestionViewController: UIViewController {
@@ -18,14 +19,22 @@ class QuestionViewController: UIViewController {
     let playButton = PButton(text: "Play", titleColor: UIColor.white, backgroundColor: Colors.base, hasBorder: true)
     let answerButton = PButton(text: "Answer", titleColor: UIColor.white, backgroundColor: Colors.base, hasBorder: true)
     
-    var recordingSession:AVAudioSession!
+    var audioSession:AVAudioSession!
+    let audioEngine = AVAudioEngine()
+    let speechRecognizer = SFSpeechRecognizer()
+    var recognitionTask:SFSpeechRecognitionTask?
+    var recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+
+    
+    var bestResult = ""
+//    let request = SFSpeechAudioBufferRecognitionRequest()
+//    var recognitionTask:SFSpeechRecognitionTask?
     
     init(question: String, key: String, index: Int) {
         self.question = question
         self.key = key
         self.index = index
         super.init(nibName: nil, bundle: nil)
-        
     }
     
     required init?(coder: NSCoder) {
@@ -35,9 +44,19 @@ class QuestionViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        var audioSession: AVAudioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(AVAudioSession.Category.playAndRecord, mode: .spokenAudio, options: .defaultToSpeaker)
+            try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+        } catch let error{
+            print("audioSession properties weren't set because of an error: ", error)
+        }
+        
         self.playButton.addTarget(self, action: #selector(QuestionViewController.playQuestion), for: .touchUpInside)
-        self.answerButton.addTarget(self, action: #selector(QuestionViewController.answerQuestion), for: .touchUpInside)
+        self.answerButton.addTarget(self, action: #selector(QuestionViewController.recordAnswer), for: .touchUpInside)
         self.view.backgroundColor = UIColor.white
+        
+        self.requestSpeechAuthorization()
         
         self.view.addSubview(self.playButton)
         self.view.addSubview(self.answerButton)
@@ -53,11 +72,11 @@ class QuestionViewController: UIViewController {
     }
     
     @objc func playQuestion() {
-        self.recordingSession = AVAudioSession.sharedInstance()
+        self.audioSession = AVAudioSession.sharedInstance()
         do {
-            try self.recordingSession.setCategory(.playAndRecord, options: .defaultToSpeaker)
-            try self.recordingSession.setActive(true, options: .notifyOthersOnDeactivation)
-            self.recordingSession.requestRecordPermission { allowed in
+            try self.audioSession.setCategory(.playAndRecord, options: .defaultToSpeaker)
+            try self.audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+            self.audioSession.requestRecordPermission { allowed in
             }
         } catch {
         }
@@ -69,8 +88,73 @@ class QuestionViewController: UIViewController {
         synthesizer.speak(utterance)
     }
     
-    @objc func answerQuestion() {
+    @objc func recordAnswer() {
         
     }
+        
+    @objc func answerTapped() {
+        if self.audioEngine.isRunning {
+            self.audioEngine.stop()
+            self.audioEngine.inputNode.removeTap(onBus: 0)
+            self.recognitionRequest.endAudio()
+            self.recognitionTask?.cancel()
+            self.answerButton.setTitle("Start Answering", for: .normal)
+            print("BEST RESULT IS \(self.bestResult)")
+        } else {
+            recordAndRecognizeSpeech()
+            self.answerButton.setTitle("Finish Answering", for: .normal)
+        }
+    }
+    
+    func recordAndRecognizeSpeech() {
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(AVAudioSession.Category.record)
+            try audioSession.setMode(.measurement)
+            try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+        } catch {
+            print("audioSession properties weren't set because of an error.")
+        }
+
+        let node = self.audioEngine.inputNode
+        let recordingFormat = node.outputFormat(forBus: 0)
+        self.recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        node.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { buffer, _ in
+            self.recognitionRequest.append(buffer)
+        }
+        self.audioEngine.prepare()
+        do {
+            try self.audioEngine.start()
+        } catch {
+            return print(error)
+        }
+        guard let recognizer = SFSpeechRecognizer() else {
+            return
+        }
+        if !recognizer.isAvailable {
+            return
+        }
+        recognitionTask = speechRecognizer?.recognitionTask(with: recognitionRequest, resultHandler: { result, error in
+            if result != nil {
+                if let result = result {
+                    let bestString = result.bestTranscription.formattedString
+                    print(bestString)
+                } else if let error = error {
+                    print(error)
+                }
+            }
+        })
+    }
+    
+    func requestSpeechAuthorization() {
+        SFSpeechRecognizer.requestAuthorization { authStatus in
+        }
+    }
+    
+}
+
+extension QuestionViewController: SFSpeechRecognizerDelegate {
+    
+    
     
 }
